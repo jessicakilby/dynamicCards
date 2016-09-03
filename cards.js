6434dc6abd5fcc75e2b3f1e7eb536e1e2e6f0d63
jessica kilby <jessicakilby@gmail.com>
2016-09-03 12:55:09 -0500

    update-still can't delete

--- cards.js
+++ cards.js
@@ -4,14 +4,28 @@ var outputText = document.getElementById("output");
 
 function createCard(){
 	var inputDiv = inputText.value;
-	outputText.innerHTML += `<div id="${inputDiv}">${inputDiv}<button onclick="deleteCard()" id="delete">Delete Card</button></div>`;
-	console.log(inputDiv);
-}
+	outputText.innerHTML += `<div id="${inputDiv}">${inputDiv}</div>`;
+	console.log("new div ID: "+inputDiv);
 
-function deleteCard(){
-	var parent = document.getElementById("output");
-	var child = document.getElementsByTagName("div");
-	parent.parentNode.removeChild(child); 
+}
+	// var deleteButton = document.getElementById("delete");
+	// deleteButton.addEventListener("click", function(){
+	// 	var deleteCardDiv = event.target.parentNode;
+	// 	console.log(deleteCardDiv);
+	// 	if(deleteCardDiv.parentNode){
+	// 		deleteCardDiv.parentNode.removeChild(deleteCardDiv);
+	// 	}
+	// });
+
+function deleteCard(e){
+	console.log(e.target);
+	// var parent = document.getElementById("output");
+	var parent = document.getElementsByTagName("div");
+	var child = document.getElementById("delete");
+	if (parent.parentNode) {
+		parent.parentNode.removeChild(child); 
+	}	
+
 	//Failed to execute 'removeChild' on 'Node': parameter 1 is not of type 'Node'
 	console.log("deleted");
 }
