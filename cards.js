265db1a118f33ad5853e52315a8a59820e05a997
jessica kilby <jessicakilby@gmail.com>
2016-09-07 06:42:14 -0500

    finally works!

--- cards.js
+++ cards.js
@@ -1,34 +1,29 @@
 var inputText = document.getElementById("input");
 var createButton = document.getElementById("create");
 var outputText = document.getElementById("output");
+var deleteListener = document.getElementsByClassName("delete");
+var child = document.getElementsByTagName("div");
 
 function createCard(){
 	var inputDiv = inputText.value;
-	outputText.innerHTML += `<div id="${inputDiv}">${inputDiv}</div>`;
-	console.log("new div ID: "+inputDiv);
-
+	outputText.innerHTML += "<div>" + inputDiv + "<button class='delete'>Delete Card</button></div>";
+	deleteListenerEvent();
 }
-	// var deleteButton = document.getElementById("delete");
-	// deleteButton.addEventListener("click", function(){
-	// 	var deleteCardDiv = event.target.parentNode;
-	// 	console.log(deleteCardDiv);
-	// 	if(deleteCardDiv.parentNode){
-	// 		deleteCardDiv.parentNode.removeChild(deleteCardDiv);
-	// 	}
-	// });
 
-function deleteCard(e){
-	console.log(e.target);
-	// var parent = document.getElementById("output");
-	var parent = document.getElementsByTagName("div");
-	var child = document.getElementById("delete");
-	if (parent.parentNode) {
-		parent.parentNode.removeChild(child); 
-	}	
-
-	//Failed to execute 'removeChild' on 'Node': parameter 1 is not of type 'Node'
-	console.log("deleted");
+function deleteCard(event){
+	var currentCard = event.target.parentElement;
+	outputText.removeChild(currentCard);
 }
+
+function deleteListenerEvent(){
+	for (var i = 0; i < deleteListener.length; i++) {
+		var deleteEvent = document.getElementsByClassName("delete");
+		for (var h = 0; h < deleteEvent.length; h++) {
+			deleteEvent[h].addEventListener("click", deleteCard);
+		}
+	}
+}	
+
 
 function enterKeyPressed(keypress){
 	if (keypress.which === 13) {
